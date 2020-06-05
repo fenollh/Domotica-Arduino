cd15c0fccc611754efbd80d5117a0d994392689d
fenollhUbuntu <fenollh@gmail.com>
2020-06-05 11:23:21 +0200

    icon bug solved

--- appReact1/src/components/section.js
+++ appReact1/src/components/section.js
@@ -1,39 +1,41 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
-import { Icon } from 'react-native-vector-icons'
+
+import Icon from 'react-native-vector-icons/Ionicons';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 export default class Section extends React.Component {
     constructor(props){
         super(props)
         this.state={
-            type:this.props.type,
+            type: this.props.type,
+            name: 'hola',
         }
     }
 
     render(){
-        console.log(typeof this.state.type)
-        console.log(this.state.type)
-/*
         let typeIcon
         switch (this.state.type) {
             case 'energia':
-                typeIcon=<SimpleLineIcons name='energy'/>
+                typeIcon=<SimpleLineIcons name='energy' size={30} color='rgb(235,235,0)' />
                 break
             case 'quimica':
-                typeIcon=<FontAwesome5 name='radiation'/>
+                typeIcon=<FontAwesome5 name='radiation' size={30} color='rgb(0,255,0)'/>
                 break
             case 'mecanica':
-                typeIcon=<FontAwesome5 name='warehouse'/>
+                typeIcon=<FontAwesome5 name='warehouse' size={30} color='rgb(0,0,0)'/>
                 break
         }
-        */
 
         return(
             <View style ={styles.container}>
                 <View style={styles.header}>
-                    <Icon name='sc-telegram' color='green'/>
+                    {typeIcon}
                 </View>
-                <View style={styles.body}></View>
+                <View style={styles.body}>
+                    {this.state.body}
+                </View>
             </View>
         )
     }
